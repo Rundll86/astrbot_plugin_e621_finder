f6f21ea6f70a2da8548480e48ae773f02ecba3e9
陨落基围虾 <3161880837@qq.com>
2026-01-11 17:33:09 +0800

    fix: 修正 create_data_path 函数中 exist_ok 参数的值
    将 exist_ok=true 改为 exist_ok=True 以使用正确的布尔值常量

--- utils.py
+++ utils.py
@@ -4,7 +4,6 @@ from typing import TypeVar
 
 import astrbot.api.message_components as Comp
 import httpx
-from sympy import true
 
 from .constants import INITIAL_GROUP_DATA, PLUGIN_DATA_PATH, RATING_LEVEL
 
@@ -43,7 +42,7 @@ def compose_rating_map(separator: str = ",\n"):
 
 
 def create_data_path():
-    os.makedirs(PLUGIN_DATA_PATH, exist_ok=true)
+    os.makedirs(PLUGIN_DATA_PATH, exist_ok=True)
 
 
 def get_group_data_path(group: str) -> str:
